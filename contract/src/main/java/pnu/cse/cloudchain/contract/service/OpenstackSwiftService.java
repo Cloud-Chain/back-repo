b36d8dadc28ea29e564ccd70becbafe88a88c81e
jaehanbyun <awbrg789@naver.com>
2023-10-05 18:00:03 +0900

    Fix: Contract Service S3 설정 제거

--- contract/src/main/java/pnu/cse/cloudchain/contract/service/OpenstackSwiftService.java
+++ contract/src/main/java/pnu/cse/cloudchain/contract/service/OpenstackSwiftService.java
@@ -19,7 +19,7 @@ import java.io.InputStream;
 
 @FeignClient(name = "openstackSwift", url = "http://10.125.70.26:38080")
 public interface OpenstackSwiftService {
-    @RequestMapping(value = "/v1/AUTH_fdb3422adae2475cac7558959244c770/usedcar/{object}"
-            , method = RequestMethod.PUT)
-    Response upload(@PathVariable("object") String object, @RequestBody byte [] input, @RequestHeader HttpHeaders httpHeaders);
+    @RequestMapping(value = "/v1/AUTH_fdb3422adae2475cac7558959244c770/usedcar/{object}", method = RequestMethod.PUT)
+    Response upload(@PathVariable("object") String object, @RequestBody byte[] input,
+            @RequestHeader HttpHeaders httpHeaders);
 }
